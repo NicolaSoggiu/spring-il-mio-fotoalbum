bd41ab540c7078b0f5d69fabc229fe6badb45867
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-11-23 10:14:06 +0100

    add class Photo

---
+++ src/main/java/com/example/springilmiofotoalbum/model/Photo.java
@@ -0,0 +1,73 @@
+package com.example.springilmiofotoalbum.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "photos")
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotBlank(message = "Title must not be blank!")
+    private String title;
+    @NotBlank(message = "Description must not be blank!")
+    private String description;
+    @NotBlank(message = "Url must not be blank!")
+    private String url;
+    private Boolean visible = false;
+    @CreationTimestamp
+    private LocalDate createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+}
